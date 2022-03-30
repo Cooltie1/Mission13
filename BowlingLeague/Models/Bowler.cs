d3a97755401296f9d159242243823bef92438d37
Scott Olson <scottolsonofficial@gmail.com>
2022-03-30 12:25:43 -0600

    Added Model Validation

--- BowlingLeague/Models/Bowler.cs
+++ BowlingLeague/Models/Bowler.cs
@@ -11,15 +11,23 @@ namespace BowlingLeague.Models
         [Key]
         [Required]
         public int BowlerID { get; set; }
+        [Required(ErrorMessage = "Please Enter their last name")]
         public string BowlerLastName { get; set; }
+        [Required(ErrorMessage = "Please Enter their first name")]
         public string BowlerFirstName { get; set; }
         public string BowlerMiddleInit { get; set; }
+        [Required(ErrorMessage = "Please Enter their city")]
         public string BowlerCity { get; set; }
+        [Required(ErrorMessage = "Please Enter their address")]
         public string BowlerAddress { get; set; }
+        [Required(ErrorMessage = "Please Enter their state")]
         public string BowlerState { get; set; }
+        [Required(ErrorMessage = "Please Enter their zipcode")]
         public string BowlerZip { get; set; }
+        [Required(ErrorMessage = "Please Enter their phonenumber")]
         public string BowlerPhoneNumber { get; set; }
-        public int TeamID { get; set; }
+        [Required(ErrorMessage = "Please select their team")]
+        public int? TeamID { get; set; }
         public Team Team { get; set; }
 
     }
